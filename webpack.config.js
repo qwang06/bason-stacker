036e24577f67adf416ddd096f4cba5df3913a19a
Qwang <qiangsemail@gmail.com>
2024-10-13 13:38:08 -0600

    rename back to public

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
 	},
 	output: {
 		filename: 'app.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'public')
 	},
 	devtool: "source-map",
 	plugins: [
